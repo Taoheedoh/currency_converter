dd87b0065940285493b1d6d7be0d2b674878af5e
Taoheedoh <taoheedoh@gmail.com>
2024-07-27 17:19:46 +0100

    Add a currency page

---
+++ lib/currency_convert_material_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class CurrencyConverterMaterialPage extends StatelessWidget {
+  const CurrencyConverterMaterialPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        
+        body: Center(
+          child: Column(
+            children: [
+              Text('0'),
+            ],
+          ),
+        ),
+      );
+  }
+}
