140a6770df37f0382f6e140cd507957f22bcd751
Taoheedoh <taoheedoh@gmail.com>
2024-07-27 17:58:17 +0100

    Add text styling

--- lib/currency_convert_material_page.dart
+++ lib/currency_convert_material_page.dart
@@ -6,14 +6,22 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        
-        body: Center(
-          child: Column(
-            children: [
-              Text('0'),
-            ],
-          ),
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '0',
+              style: TextStyle(
+                fontSize: 45,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+          ],
         ),
-      );
+      ),
+    );
   }
 }
